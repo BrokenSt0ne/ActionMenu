5ab9d035200d5865c2496ad3f73ad73103f6c331
BrokenSt0ne <105749252+BrokenSt0ne@users.noreply.github.com>
2024-07-13 21:33:17 +0100

    more src cuz i forgot

---
+++ Menu/MenuHandler.cs
@@ -0,0 +1,39 @@
+﻿using BepInEx;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using System.Text;
+using UnityEngine;
+using Utilla;
+
+namespace ActionMenu.Menu
+{
+    public class MenuHandler : MonoBehaviour
+    {
+        /*public void Awake()
+        {
+            Debug.Log("!!!!! trying to load ongameinit !!!!!");
+            Utilla.Events.GameInitialized += OnGameInitialized;
+        }
+
+        public AssetBundle LoadAssetBundle(string path)
+        {
+            Stream stream = Assembly.GetExecutingAssembly().GetManifestResourceStream(path);
+            AssetBundle bundle = AssetBundle.LoadFromStream(stream);
+            stream.Close();
+            return bundle;
+        }
+
+        private void OnGameInitialized(object sender, EventArgs e)
+        {
+            Debug.Log("!!!!! it loaded ongameinit !!!!!");
+            var ActionMenuAssets = LoadAssetBundle("ActionMenu.Resources.actionmenu");
+            var ActionMenu = UnityEngine.Object.Instantiate(ActionMenuAssets.LoadAsset<GameObject>("ActionMenu_Parent"));
+            UnityEngine.Object.Instantiate(ActionMenuAssets.LoadAsset<GameObject>("ActionMenu_Parent"));
+            ActionMenu.transform.position = Vector3.zero;
+            ActionMenu.transform.rotation = Quaternion.identity;
+            ActionMenu.transform.localScale = Vector3.one;
+        }*/
+    }
+}
---
+++ Utilities/ControllerInput.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+
+namespace ActionMenu.Utilities
+{
+    internal class ControllerInput : MonoBehaviour
+    {
+
+    }
+}
